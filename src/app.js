df04a066543991c4fe92593958c0a839cf157687
Stefan Geisbacher <s.geisbacher@netconomy.net>
2016-07-02 16:33:53 +0200

    extended cyclejs-example

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import Rx from 'rxjs/Rx';
 import Cycle from '@cycle/rxjs-run';
-import {h1, makeDOMDriver} from '@cycle/dom';
+import {h1, div, hr, span, makeDOMDriver} from '@cycle/dom';
 //import {makeHistoryDriver} from '@cycle/history'
 //import {createHistory} from 'history';
 //import storageDriver from '@cycle/storage';
 
 function main(sources) {
+  const clicks$ = sources.DOM.select('#app').events('click')
+    .map(ev => 1)
+    .startWith(0)
+    .scan((total, curr) => total + curr);
+
   return {
-    DOM: Rx.Observable.of('CycleJS with RxJS5 works!')
-           .map(txt => h1(txt))
+    DOM: clicks$
+           .map(total =>
+                   div([
+                       h1('CycleJS with RxJS5 works!'),
+                       hr(),
+                       span(`${total} times clicked!`)
+                    ]))
   };
 }
 
